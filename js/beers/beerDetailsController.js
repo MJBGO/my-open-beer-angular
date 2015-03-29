9befc6138875225193126351b6cc57925b96dd5d
Jean-Baptiste GOMOND <dev@jbgomond.com>
2015-03-29 03:24:50 +0200

    Activated button "Show brewery" on beer page. The name of the brewery is now shown on this page.

--- js/beers/beerDetailsController.js
+++ js/beers/beerDetailsController.js
@@ -1,5 +1,5 @@
-module.exports=function($scope,config,$location) {
-
+module.exports=function($scope,config,$location,rest) {
+    $scope.data = {};
     $scope.beer = config.activeBeer;
 
     if (angular.isUndefined(config.activeBeer)) {
@@ -8,5 +8,20 @@ module.exports=function($scope,config,$location) {
         if ($scope.beer.photo == null || $scope.beer.photo == "") {
             $scope.beer.photo = "beer.png";
         }
+    }
+
+    var breweryUrl = "breweries/" + config.activeBeer.idBrewery;
+    rest.getAll($scope.data, breweryUrl);
+
+    $scope.getBreweryName = function() {
+        return $scope.data[breweryUrl].name;
+    };
+
+    $scope.showBrewery = function() {
+        if(angular.isDefined($scope.data[breweryUrl])) {
+            config.activeBrewery = angular.copy($scope.data[breweryUrl]);
+            $location.path("breweries/details");
+        }
+
     }
 };
--- js/beers/beersModule.js
+++ js/beers/beersModule.js
@@ -1,6 +1,6 @@
 var appBeers = angular.module("BeersApp", []).
 controller("BeersController", ["$scope","rest","$timeout","$location","config","$route","save","user", require("./beersController")]).
 controller("BeerAddController",["$scope","config","$location","rest","save","$document","modalService","user",require("./beerAddController")]).
-controller("BeerDetailsController",["$scope","config","$location",require("./beerDetailsController")]).
+controller("BeerDetailsController",["$scope","config","$location","rest",require("./beerDetailsController")]).
 controller("BeerUpdateController",["$scope","config","$location","rest","save","$document","modalService","$controller","user",require("./beerUpdateController")]);
 module.exports=angular.module("BeersApp").name;
